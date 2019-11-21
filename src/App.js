a330bb69339152967c76f016786ec4a9bb605150
Daniel Field <danielfield3515@gmail.com>
2019-11-21 14:46:23 +1000

    404 page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
+/* Components */
+import PageNotFound from './components/PageNotFound';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <div className="container">
+                    <Switch>
+                        {/* <Route exact path="/register" component={Register} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/profile" component={Profile} />
+                        <Route exact path="/logout" component={Logout} />
+                        <Route exact path="/" component={Home} /> */}
+                        <Route component={PageNotFound} />
+                    </Switch>
+
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class PageNotFound extends Component {
+    render() {
+        return (
+            <h1 className="pagenotfound">The page you requested doesn't exist!</h1>
+        );
+    }
+}
+export default PageNotFound;
